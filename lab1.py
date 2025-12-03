8807998f8956cec178baec98bf619b3333640251
Nadaaa72 <C24317713@myTUDublin.ie>
2025-12-03 17:00:00 +0000

    lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,4 @@
+#variable exercise: red circle that changes size dynamically
 import py5
 
 # starting size and direction
@@ -23,3 +24,17 @@ def draw():
         size -= 2   # shrink
         if size <= 30:    # min size
             grow = True
+
+
+# variable exercise: green circle
+import py5
+
+def setup():
+    py5.size(500, 500)
+    py5.background(0)  # black background
+
+def draw():
+    py5.background(0)        # keep background black each frame
+    py5.fill(57, 255, 20)    # neon green color
+    py5.no_stroke()          # remove outline
+    py5.circle(py5.mouse_x, py5.mouse_y, 40)  # circle follows the mouse
